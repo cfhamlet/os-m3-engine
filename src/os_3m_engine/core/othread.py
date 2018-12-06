2fcb586a643f5394bce510ee7e0262e3b5794007
Ozzy <cfhamlet@gmail.com>
2018-12-06 12:02:09 +0800

    update: use Event as stop trigger

--- src/os_3m_engine/core/othread.py
+++ src/os_3m_engine/core/othread.py
@@ -9,13 +9,13 @@ from .driver import Driver
 class Othread(Configurable, threading.Thread):
 
     def __init__(self, engine_config, runtime_context,
-                 start_trigger, component_factory, **kwargs):
+                 component_factory, start_trigger, stop_trigger, **kwargs):
         Configurable.__init__(self, engine_config)
         threading.Thread.__init__(self, **kwargs)
         self._driver_cls = load_class(engine_config.driver_cls, Driver)
         self._component_factory = component_factory
         self._runtime_context = runtime_context
-        self._stopping = False
+        self._stop_trigger = stop_trigger
         self._start_trigger = start_trigger
         self._logger = logging.getLogger(self.name)
 
@@ -39,23 +39,24 @@ class Othread(Configurable, threading.Thread):
                     driver.cleanup()
             except Exception as e:
                 self._logger.error('Unexpected exception, %s' % str(e))
+        self._stop_trigger.set()
         self._logger.debug('Stop')
 
     def stopping(self):
-        return self._stopping
-
-    def stop(self):
-        self._stopping = True
+        return self._stop_trigger.is_set()
 
 
 class OthreadManager(Configurable):
     def __init__(self, engine_config, runtime_context, component_factory):
         super(OthreadManager, self).__init__(engine_config)
         start_trigger = threading.Event()
+        stop_trigger = threading.Event()
         self._start_trigger = start_trigger
+        self._stop_trigger = stop_trigger
         thread_cls = load_class(engine_config.thread_cls, Othread)
         self._threads = [thread_cls(
-            engine_config, runtime_context, start_trigger, component_factory,
+            engine_config, runtime_context, component_factory,
+            start_trigger, stop_trigger,
             name='%s-%d' % (thread_cls.__name__, i))
             for i in range(0, engine_config.thread_num)]
 
@@ -76,7 +77,7 @@ class OthreadManager(Configurable):
         list(map(lambda t: t.join(), self._threads))
 
     def stop(self):
-        list(map(lambda t: t.stop(), self._threads))
+        self._stop_trigger.set()
 
     def stopped(self):
         return not any([t.isAlive() for t in self._threads])
