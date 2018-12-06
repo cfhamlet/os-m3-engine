442b972b4652b80cd68b8cbd95f6dba0fad38002
Ozzy <cfhamlet@gmail.com>
2018-12-06 15:39:54 +0800

    test: auto tune queue size

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -21,14 +21,16 @@ def test_create_001():
 def test_create_002():
     with pytest.raises(ValueError):
         create(frontend_cls=None)
+    with pytest.raises(ValueError):
+        create(transport_cls=None, backend_cls=None)
 
 
 def test_create_003(config):
     config.queue_size = 1000
     config.thread_num = 10
     runtime_context = RuntimeContext()
-    _ = create(frontend_engine_config=config,
-               backend_engine_config=config,
+    _ = create(engine_frontend_config=config,
+               engine_backend_config=config,
                runtime_context=runtime_context)
     assert runtime_context.frontend_thread_queue.maxsize == config.queue_size
     assert runtime_context.frontend_thread.thread_num == config.thread_num
@@ -51,3 +53,40 @@ def test_create_005(config):
     _ = create(backend_cls=None, runtime_context=runtime_context)
     for attr in ['backend_thread', 'backend_thread_queue']:
         assert not hasattr(runtime_context, attr)
+
+
+def test_create_006(config):
+    runtime_context = RuntimeContext()
+    config.thread_num = 10
+    _ = create(engine_frontend_config=config, runtime_context=runtime_context)
+    assert runtime_context.frontend_thread_queue.maxsize > 10
+
+
+def test_create_007(config):
+    runtime_context = RuntimeContext()
+    config.queue_size = 10
+    _ = create(engine_frontend_config=config, runtime_context=runtime_context)
+    assert runtime_context.frontend_thread_queue.maxsize == 10
+
+
+def test_create_008(config):
+    runtime_context = RuntimeContext()
+    config.thread_num = 10
+    _ = create(engine_backend_config=config, runtime_context=runtime_context)
+    assert runtime_context.backend_thread_queue.maxsize > 10
+
+
+def test_create_009(config):
+    runtime_context = RuntimeContext()
+    config.thread_num = 10
+    _ = create(transport_cls=None, engine_backend_config=config,
+               runtime_context=runtime_context)
+    assert runtime_context.frontend_thread_queue.maxsize > 10
+
+
+def test_create_010(config):
+    runtime_context = RuntimeContext()
+    config.thread_num = 10
+    _ = create(backend_cls=None, engine_transport_config=config,
+               runtime_context=runtime_context)
+    assert runtime_context.frontend_thread_queue.maxsize > 10
